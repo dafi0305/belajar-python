06a626209c2fcad4600e4b72091ce66453439a33
dafi0305 <dikssumual@gmail.com>
2025-10-31 08:26:23 +0700

    Add files via upload

---
+++ variabel.py
@@ -0,0 +1,28 @@
+#nama = "dafi"
+#mapel_fav = "b.indo"
+#nilai = 88
+#perempuan = False
+
+#print("===Data Nilai===")
+#print("nama: %s" %  (nama))
+#print("mapel_fav: %s, nilai: %dn, perempuan: %r" %(mapel_fav, nilai, perempuan))
+
+barang1 = "roti"
+barang2 = "susu"
+barang3 = "selai"
+barang4 = "gula"
+barang5 = "kopi"
+harga1 = 10000
+harga2 = 15000
+harga3 = 17000
+harga4 = 22000
+harga5 = 8000
+total = harga1 + harga3
+print("===selamat datang di kokomart===")
+print("1.%s-RP%d" %(barang1, harga1))
+print("2.%s-RP%d" %(barang2, harga2))
+print("3.%s-RP%d" %(barang3, harga3))
+print("4.%s-RP%d" %(barang4, harga4))
+print("5.%s-RP%d" %(barang5, harga5))
+print("Total Belanja: %s+%s=" %(barang1, barang3)) 
+print(total)
